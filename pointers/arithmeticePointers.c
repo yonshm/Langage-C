d324e4e55225cee9fba63579e3690934511a47f7
youness <younesshamou013@gmail.com>
2025-07-18 21:13:45 +0100

    arithmetic pointers

--- pointers/arithmeticePointers.c
+++ pointers/arithmeticePointers.c
@@ -2,7 +2,9 @@
 
 int main(){
     int x = 5;
+    int y = 10;
     int *ptrX = &x;
+    int *ptrY = &y;
     printf("Adress memory of x: %u\n", ptrX);
     ptrX++; // ptrx = ptrx + 4 / 4bytes will be added
     printf("Adress memory of x++: %u\n", ptrX);
@@ -15,4 +17,8 @@ int main(){
 
     ptrX-=5; // ptrx = ptrx - sizeOf(int) * 5;
     printf("Adress memory of x - 5: %u\n", ptrX);
+
+    printf("Substraction of two pointers\n");
+    printf("Adress memory of x: %u , Adress memory of y: %u\n", ptrX,ptrY);
+    printf("ptrX - ptrY = %d\n", ptrX-ptrY); // result = (ptrx - ptrY) / sizeOf(int);  
 }
---
+++ pointers/arraysPointer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int *ptr;
+    ptr = arr;
+    printf("%d", arr);
+}
